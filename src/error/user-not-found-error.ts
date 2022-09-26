6c40b40b81c9025f98c2c9698c83b8d1528e45bb
TURMEL Kevin <kevinturmel@gmail.com>
2022-09-26 12:38:18 +0200

    chore: remove user-not-found-error.ts

--- src/error/user-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseError } from 'firebase/app';
-
-export class UserNotFoundError extends FirebaseError {
-  constructor(firebaseError: FirebaseError) {
-    super(firebaseError.code, firebaseError.message, firebaseError.customData);
-  }
-}
