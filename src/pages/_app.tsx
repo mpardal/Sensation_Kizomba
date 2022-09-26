b072cfa0265b50f4d9ee951d123e04314251b2d9
TURMEL Kevin <kevinturmel@gmail.com>
2022-09-26 11:59:54 +0200

    feat: sign up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import { QueryClient } from '@tanstack/query-core';
-import { QueryClientProvider } from '@tanstack/react-query';
-import MuiTheme from '../components/mui-theme';
-import FirebaseAuthProvider from '../hooks/auth/use-auth';
-import GlobalSnackbarProvider from '../hooks/use-global-snackbar';
-import type { NextPageWithLayout } from '../components/layout';
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+import MuiTheme from "../components/mui-theme";
+import FirebaseAuthProvider from "../hooks/auth/use-auth";
+import GlobalSnackbarProvider from "../hooks/use-global-snackbar";
+import type { NextPageWithLayout } from "../components/layout";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -14,9 +14,9 @@ type AppPropsWithLayout = AppProps & {
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
+      refetchOnWindowFocus: false
+    }
+  }
 });
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
