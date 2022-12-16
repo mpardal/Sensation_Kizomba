e24dcccbc242a3cae2e838f4489dbb1d0217be9a
mpardal <103637557+mpardal@users.noreply.github.com>
2022-12-16 11:49:11 +0100

    create homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,36 @@
 import { Typography } from '@mui/material';
 import React from 'react';
+import slugify from 'slugify';
+import { useNextEvents } from '@/hooks/use-next-events';
+import Event from '@/components/event';
+import type { NextPageWithLayout } from '@/components/layout';
 import Layout from '../components/layout';
-import type { NextPageWithLayout } from '../components/layout';
 
 const Home: NextPageWithLayout = () => {
+  const nextEvents = useNextEvents();
   return (
-    <Typography className="text-center" component="h1" variant="h3">
-      Derniers articles
-    </Typography>
+    <div>
+      <Typography className="text-center mb-10" component="h1" variant="h3">
+        Prochains événements
+      </Typography>
+      {nextEvents.isSuccess ? (
+        <div className="flex flex-col w-3/4 mx-auto grow overflow-auto gap-3">
+          {nextEvents.data.docs.map((event) => {
+            const data = event.data();
+
+            return (
+              <Event
+                address={data.address}
+                date={data.date}
+                key={event.id}
+                linkDetails={`/events/${slugify(data.title)}/${event.id}`}
+                title={data.title}
+              />
+            );
+          })}
+        </div>
+      ) : null}
+    </div>
   );
 };
 
