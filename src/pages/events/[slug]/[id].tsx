968023433a11bef632b3748bf3f619447d8c587a
TURMEL Kevin <kevinturmel@gmail.com>
2022-12-17 23:01:00 +0100

    remove getServerSideProps for [id].tsx

--- src/pages/events/[slug]/[id].tsx
+++ src/pages/events/[slug]/[id].tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import type { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 import { useEvent } from '@/hooks/use-event';
 import type { NextPageWithLayout } from '@/components/layout';
 import Layout from '@/components/layout';
 import DetailEvent from '@/components/detail-event';
 
-interface EventPageProps {
-  //Type le paramètre id
-  id: string;
-}
+const EventPage: NextPageWithLayout = () => {
+  const router = useRouter();
 
-const EventPage: NextPageWithLayout<EventPageProps> = ({ id }) => {
+  const id = router.query.id as string | undefined;
+
   // [id].tsx - events/slug-conexao/[id].tsx
   const event = useEvent(id);
 
@@ -34,22 +34,6 @@ const EventPage: NextPageWithLayout<EventPageProps> = ({ id }) => {
 // Intègre les éléments de base de la page (graphisme)
 EventPage.Layout = function ContactLayout(page) {
   return <Layout>{page}</Layout>;
-};
-
-//fonction provenant de Nextjs
-export const getServerSideProps: GetServerSideProps<EventPageProps> = async (
-  ctx,
-  // eslint-disable-next-line @typescript-eslint/require-await
-) => {
-  //Utiliser pour gérér la récupération des routes dynamiques
-  const id = ctx.query.id as string;
-
-  return {
-    props: {
-      //Renseigne le paramètre à renvoyer
-      id,
-    },
-  };
 };
 
 export default EventPage;
