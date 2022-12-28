57477d9c2032dc10fd33e68c2146b93cb554c475
Mickael PARDAL <103637557+mpardal@users.noreply.github.com>
2022-12-28 16:55:49 +0100

    feat: hide old event from next banner and list (#22)
    Co-authored-by: TURMEL Kevin <kevinturmel@gmail.com>

--- src/hooks/use-events.ts
+++ src/hooks/use-events.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 import type { UseQueryOptions } from '@tanstack/react-query';
 import type { QuerySnapshot } from 'firebase/firestore';
 import { database } from '@/config/firebase-config';
@@ -7,26 +7,40 @@ import type { AppEvent } from '@/types/app-event';
 import { serializeQuerySnapshot } from '@/utils/serialize-snapshot';
 import { getDownloadUrlsForEvent } from '@/utils/get-download-urls-for-event';
 
+const today = new Date(new Date().setHours(0, 0, 0, 0));
+
 export function getEventsQueryKey(city?: string) {
   return ['events', city];
 }
 
 export async function fetchEvents(city?: string): Promise<AppEvent[]> {
   const queryEvents = city
-    ? query(collection(database, 'events'), where('city', '==', city)) //Tri la recherche par la ville
-    : query(collection(database, 'events')); //Exécute la fonction même s'il n'y a pas de tri par ville
+    ? query(
+        collection(database, 'events'),
+        //Tri la recherche par la ville
+        where('city', '==', city),
+        orderBy('date.from', 'asc'),
+      )
+    : query(collection(database, 'events'), orderBy('date.from', 'asc')); //Exécute la fonction même s'il n'y a pas de tri par ville
 
   const eventsQuerySnapshot = (await getDocs(
     queryEvents,
   )) as QuerySnapshot<AppEvent>;
 
   return Promise.all(
-    serializeQuerySnapshot(eventsQuerySnapshot).map(async (event) => {
-      return {
-        ...event,
-        images: await getDownloadUrlsForEvent(event),
-      };
-    }),
+    serializeQuerySnapshot(eventsQuerySnapshot)
+      .filter((evt) => {
+        const from = new Date(evt.date.from);
+        const to = evt.date.to ? new Date(evt.date.to) : undefined;
+
+        return from >= today || (to && to >= today);
+      })
+      .map(async (event) => {
+        return {
+          ...event,
+          images: await getDownloadUrlsForEvent(event),
+        };
+      }),
   );
 }
 
--- src/hooks/use-next-events.ts
+++ src/hooks/use-next-events.ts
@@ -1,20 +1,13 @@
 import type { UseQueryOptions } from '@tanstack/react-query';
 import type { QuerySnapshot } from 'firebase/firestore';
 import { useQuery } from '@tanstack/react-query';
-import {
-  getDocs,
-  query,
-  collection,
-  orderBy,
-  limit,
-  where,
-} from 'firebase/firestore';
+import { getDocs, query, collection, orderBy } from 'firebase/firestore';
 import type { AppEvent } from '@/types/app-event';
 import { database } from '@/config/firebase-config';
 import { serializeQuerySnapshot } from '@/utils/serialize-snapshot';
 import { getDownloadUrlsForEvent } from '@/utils/get-download-urls-for-event';
 
-const today = new Date();
+const today = new Date(new Date().setHours(0, 0, 0, 0));
 
 export function getNextEventsQueryKey() {
   return ['next-events'];
@@ -23,9 +16,7 @@ export function getNextEventsQueryKey() {
 export async function fetchNextEvents() {
   const nextEventsQuery = query(
     collection(database, 'events'),
-    where('date.from', '>', today),
     orderBy('date.from', 'asc'),
-    limit(3),
   );
 
   const nextEventsSnapshot = (await getDocs(
@@ -33,12 +24,19 @@ export async function fetchNextEvents() {
   )) as QuerySnapshot<AppEvent>;
 
   return Promise.all(
-    serializeQuerySnapshot(nextEventsSnapshot).map(async (event) => {
-      return {
-        ...event,
-        images: await getDownloadUrlsForEvent(event),
-      };
-    }),
+    serializeQuerySnapshot(nextEventsSnapshot)
+      .filter((evt) => {
+        const from = new Date(evt.date.from);
+        const to = evt.date.to ? new Date(evt.date.to) : undefined;
+
+        return from >= today || (to && to >= today);
+      })
+      .map(async (event) => {
+        return {
+          ...event,
+          images: await getDownloadUrlsForEvent(event),
+        };
+      }),
   );
 }
 
