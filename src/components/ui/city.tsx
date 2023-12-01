1d4c7568bd0de0a851ece87898759781348c0c8b
mpardal@vpauto.eu <mpardal@vpauto.eu>
2023-12-01 19:59:21 +0100

    change display city

--- src/components/ui/city.tsx
+++ src/components/ui/city.tsx
@@ -10,6 +10,8 @@ import React from 'react';
 import { useEvents } from '@/hooks/use-events';
 import { cityKeyToCityName } from '@/utils/city-key-to-city-name';
 import type { AppEvent } from '@/types/app-event';
+import EventCard from '@/components/event-card';
+import { slugifyEventLink } from '@/utils/slugify-event-link';
 import EventsCardList from '../events-card-list';
 
 function City({ city, events }: { city: string; events?: AppEvent[] }) {
@@ -31,67 +33,20 @@ function City({ city, events }: { city: string; events?: AppEvent[] }) {
         ) : null}
 
         <Fade in={eventsQuery.isSuccess}>
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-x-12 gap-y-8 sm:gap-y-12">
-            {eventsQuery.data ? (
-              <>
-                <Card className="flex flex-col">
-                  <CardHeader
-                    title="Événements hebdomadaires"
-                    titleTypographyProps={{ variant: 'h4', component: 'h4' }}
+          <div className="grid grid-cols-1 gap-x-12 gap-y-8 sm:gap-y-12">
+            {eventsQuery.isSuccess ? (
+              <div className="flex flex-col w-full mx-auto grow overflow-auto gap-3 px-4">
+                {eventsQuery.data.map((event) => (
+                  <EventCard
+                    address={event.address}
+                    date={event.date}
+                    images={event.images}
+                    key={event.id}
+                    linkDetails={slugifyEventLink(event)}
+                    title={event.title}
                   />
-
-                  <CardContent className="w-full">
-                    <EventsCardList
-                      events={eventsQuery.data.filter(
-                        (d) => d.type === 'weekly',
-                      )}
-                    />
-                  </CardContent>
-                </Card>
-
-                <Card className="flex flex-col">
-                  <CardHeader
-                    title="Événements mensuels"
-                    titleTypographyProps={{ variant: 'h4', component: 'h4' }}
-                  />
-                  <CardContent className="grow flex justify-center">
-                    <EventsCardList
-                      events={eventsQuery.data.filter(
-                        (d) => d.type === 'monthly',
-                      )}
-                    />
-                  </CardContent>
-                </Card>
-
-                <Card className="flex flex-col">
-                  <CardHeader
-                    title="Événements trimestriels"
-                    titleTypographyProps={{ variant: 'h4', component: 'h4' }}
-                  />
-
-                  <CardContent className="grow flex justify-center">
-                    <EventsCardList
-                      events={eventsQuery.data.filter(
-                        (d) => d.type === 'quarterly',
-                      )}
-                    />
-                  </CardContent>
-                </Card>
-
-                <Card className="flex flex-col">
-                  <CardHeader
-                    title="Événements annuels"
-                    titleTypographyProps={{ variant: 'h4', component: 'h4' }}
-                  />
-                  <CardContent className="grow flex justify-center">
-                    <EventsCardList
-                      events={eventsQuery.data.filter(
-                        (d) => d.type === 'yearly',
-                      )}
-                    />
-                  </CardContent>
-                </Card>
-              </>
+                ))}
+              </div>
             ) : null}
           </div>
         </Fade>
